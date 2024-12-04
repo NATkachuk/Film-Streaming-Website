498ad8dc8202f5f2362f6e83a40c3d966c3aa73e
NATkachuk <nazartkacuk301@gmail.com>
2024-12-04 15:18:31 +0300

    finish project

--- script/main.js
+++ script/main.js
@@ -43,3 +43,74 @@ minusIcons.forEach((icon, index) => {
     }
   });
 });
+
+const All = document.getElementById('All');
+const Action = document.getElementById('Action');
+const Comedy = document.getElementById('Comedy');
+const Drama = document.getElementById('Drama');
+const SiFi = document.getElementById('Sci-Fi');
+const Horror = document.getElementById('Horror');
+const Romance = document.getElementById('Romance');
+const Musical = document.getElementById('Musical');
+const box = document.getElementById('box');
+
+const originalOrder = Array.from(box.children);
+
+let isReversed = false;
+
+All.addEventListener('click', (event) => {
+  event.preventDefault();
+  restoreOriginal();
+});
+
+Action.addEventListener('click', (event) => {
+  event.preventDefault();
+  reverseCard();
+});
+
+Drama.addEventListener('click', (event) => {
+  event.preventDefault();
+  restoreOriginal();
+});
+
+Comedy.addEventListener('click', (event) => {
+  event.preventDefault();
+  reverseCard();
+});
+
+SiFi.addEventListener('click', (event) => {
+  event.preventDefault();
+  restoreOriginal();
+});
+
+Horror.addEventListener('click', (event) => {
+  event.preventDefault();
+  reverseCard();
+});
+
+Romance.addEventListener('click', (event) => {
+  event.preventDefault();
+  restoreOriginal();
+});
+
+Musical.addEventListener('click', (event) => {
+  event.preventDefault();
+  reverseCard();
+});
+
+function restoreOriginal() {
+  box.innerHTML = '';
+  originalOrder.forEach((card) => {
+    box.appendChild(card);
+  });
+  isReversed = false;
+}
+
+function reverseCard() {
+  const cards = Array.from(box.children);
+  if (!isReversed) {
+    cards.reverse().forEach((card) => box.appendChild(card));
+    isReversed = true;
+  }
+}
+
